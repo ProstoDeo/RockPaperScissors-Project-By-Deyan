367e4bddfc792d42e5d3d62f7ff860fac1b8dcd5
Deyan Ilkov <96566576+ProstoDeo@users.noreply.github.com>
2023-04-17 23:12:10 +0300

    Add files via upload

--- RockPaperScissors/RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors.cs
@@ -1,96 +1,135 @@
-﻿string Rock = "Rock";
-string Paper = "Paper";
-string Scissors = "Scissors";
-Console.ForegroundColor = ConsoleColor.Blue;
-Console.WriteLine("Choose [r]ock, [p]aper or [s]cissors: ");
-Console.ForegroundColor = ConsoleColor.White;
-string playerMove = Console.ReadLine();
-switch (playerMove)
+﻿int score = 0;
+bool playAgain = true;
+while (playAgain)
 {
-    case "r":
-    case "rock":
-        playerMove = "Rock";
-        break;
-    case "s":
-    case "scissors":
-        playerMove = "Scissors";
-        break;
-    case "p":
-    case "paper":
-        playerMove = "Paper";
-        break;
-    default:
-        Console.WriteLine("Invlaid Input. Try Again...");
-        return;
+    string winorlose = "";
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Choose [r]ock, [p]aper or [s]cissors: ");
+    Console.ForegroundColor = ConsoleColor.White;
+    string playerMove = Console.ReadLine();
+    switch (playerMove)
+    {
+        case "r":
+        case "rock":
+            playerMove = "Rock";
+            break;
+        case "s":
+        case "scissors":
+            playerMove = "Scissors";
+            break;
+        case "p":
+        case "paper":
+            playerMove = "Paper";
+            break;
+        default:
+            Console.WriteLine("Invlaid Input. Try Again...");
+            return;
+    }
+    Random random = new Random();
+    int computerRandomNumber = random.Next(1, 4);
+    string computerMove = "";
+    switch (computerRandomNumber)
+    {
+        case 1:
+            computerMove = "Rock";
+            break;
+        case 2:
+            computerMove = "Paper";
+            break;
+        case 3:
+            computerMove = "Scissors";
+            break;
+    }
+    Console.WriteLine($"The computer chose {computerMove}.");
+    switch (playerMove)
+    {
+        case "Rock":
+            switch (computerMove)
+            {
+                case "Scissors":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("You win.");
+                    winorlose = "won";
+                    break;
+                case "Rock":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("This game was a draw.");
+                    winorlose = "draw";
+                    break;
+                case "Paper":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You lose.");
+                    winorlose = "lost";
+                    break;
+            }
+            break;
+        case "Paper":
+            switch (computerMove)
+            {
+                case "Rock":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("You win.");
+                    winorlose = "won";
+                    break;
+                case "Paper":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("This game was a draw.");
+                    winorlose = "draw";
+                    break;
+                case "Scissors":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You lose.");
+                    winorlose = "lost";
+                    break;
+            }
+            break;
+        case "Scissors":
+            switch (computerMove)
+            {
+                case "Paper":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("You win.");
+                    winorlose = "won";
+                    break;
+                case "Scissors":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("This game was a draw.");
+                    winorlose = "draw";
+                    break;
+                case "Rock":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You lose.");
+                    winorlose = "lost";
+                    break;
+            }
+            break;
+    }
+    switch (winorlose)
+    {
+        case "won":
+            Console.ForegroundColor = ConsoleColor.Green;
+            score += 1;
+            break;
+        case "lose":
+            Console.ForegroundColor = ConsoleColor.Red;
+            score -= 1;
+            break;
+        case "draw":
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            break;
+        default:
+            Console.ResetColor();
+            break;
+    }
+    string ending = "";
+    if (score > 1)
+    {
+        ending = "s";
+    }
+    Console.WriteLine($"You have won {score} time{ending}.");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Do you want to play again (Yes or No)");
+    string input = Console.ReadLine();
+    playAgain = (input == "Yes" || input == "yes");
+    Console.ResetColor();
 }
-Random random = new Random();
-int computerRandomNumber = random.Next(1, 4);
-string computerMove = "";
-switch (computerRandomNumber)
-{
-    case 1:
-        computerMove = "Rock";
-        break;
-    case 2:
-        computerMove = "Paper";
-        break;
-    case 3:
-        computerMove = "Scissors";
-        break;
-}
-Console.WriteLine($"The computer chose {computerMove}.");
-switch (playerMove)
-{
-    case "Rock":
-        switch (computerMove)
-        {
-            case "Scissors":
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("You win.");
-                break;
-            case "Rock":
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("This game was a draw.");
-                break;
-            case "Paper":
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("You lose.");
-                break;
-        }
-        break;
-    case "Paper":
-        switch (computerMove)
-        {
-            case "Rock":
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("You win.");
-                break;
-            case "Paper":
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("This game was a draw.");
-                break;
-            case "Scissors":
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("You lose.");
-                break;
-        }
-        break;
-    case "Scissors":
-        switch (computerMove)
-        {
-            case "Paper":
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("You win.");
-                break;
-            case "Scissors":
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("This game was a draw.");
-                break;
-            case "Rock":
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("You lose.");
-                break;
-        }
-        break;
-}
-Console.ResetColor();
